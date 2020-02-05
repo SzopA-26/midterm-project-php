60ea343169445df5acbdf2ab72bb4d21223fe47f
bellweerin <weerin.n@ku.th>
2020-02-05 16:55:46 +0700

    update

--- src/Controllers/UsersController.php
+++ src/Controllers/UsersController.php
@@ -34,7 +34,7 @@ class UsersController extends Controller
         $gender = $input->gender;
         
 
-        if (!$username or !$email or !$password or !$c_password or !$birthdate ) {
+        if (!$username or !$email or !$password or !$c_password or !$birthdate) {
             echo "<script>alert('Please enter your information.')</script>";
         } else if (($user = (new User())->select_by_username($username))) {
             echo "<script>alert('username already exist.')</script>";

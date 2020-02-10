896e86a2420073bf9403edda1adacf15301eb7ef
Zee <patiphat.kh@ku.th>
2020-02-10 23:06:09 +0700

    fix edit

--- public/js/write.js
+++ public/js/write.js
@@ -36,5 +36,36 @@ $(document).ready(function() {
         })
     });
 
+    $('#update-story-btn').on('click', function() {
+        var title = $("#title-input").val();
+        var post_id = $("#post_id").val();
+        var contentcode = $('#summernote').summernote('code');
+        $.ajax({
+            url: '/stories/update',
+            data: {
+                title: title,
+                post_id: post_id,
+                content: contentcode,
+            },
+            method: 'POST'
+        }).done(function (message) {
+            console.log(message);
+            console.log(message == message);
+            for (let i in message) {
+                console.log(i , message[i]);
+            }
+            let test = 'success'
+            console.log(test.length);
+            
+            console.log( message == "success")
+            if (message == 'success') {
+                alert('stories saved success.')
+                window.location.href='/stories'
+            } else {
+                alert('stories cannot save.')
+            }
+        })
+    });
+
     
 })
